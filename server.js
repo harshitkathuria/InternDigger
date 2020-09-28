f29b3117925824bda6d63ec7c496795d367b8863
harshitkathuria <harshitkathuria14@gmail.com>
2020-09-28 19:18:49 +0530

    Fixed dotenv issue

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ process.on('uncaughtException', err => {
 })
 
 // Connecting env variables
-if(process.env.NODE_ENV === 'develpoment')
-  dotenv.config({ path: './config.env' });
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
